4a427abe0b3a56a06bb68cee11c88c7d2958171b
kivu8 <kivueight@gmail.com>
2014-07-18 10:04:33 +0200

    Update BasePeer.php

--- runtime/lib/util/BasePeer.php
+++ runtime/lib/util/BasePeer.php
@@ -657,200 +657,281 @@ class BasePeer
      * @throws PropelException Trouble creating the query string.
      */
     public static function createSelectSql(Criteria $criteria, &$params)
-    {
-        $db = Propel::getDB($criteria->getDbName());
-        $dbMap = Propel::getDatabaseMap($criteria->getDbName());
+	{
+		$db = Propel::getDB($criteria->getDbName());
+		$dbMap = Propel::getDatabaseMap($criteria->getDbName());
 
-        $fromClause = array();
-        $joinClause = array();
-        $joinTables = array();
-        $whereClause = array();
-        $orderByClause = array();
+		// redundant definition $selectModifiers = array();
+		$selectClause = array();
+		$fromClause = array();
+		$joinClause = array();
+		$joinTables = array();
+		$whereClause = array();
+		$orderByClause = array();
+		// redundant definition $groupByClause = array();
 
-        $orderBy = $criteria->getOrderByColumns();
-        $groupBy = $criteria->getGroupByColumns();
-        $ignoreCase = $criteria->isIgnoreCase();
+		$orderBy = $criteria->getOrderByColumns();
+		$groupBy = $criteria->getGroupByColumns();
+		$ignoreCase = $criteria->isIgnoreCase();
+		$select = $criteria->getSelectColumns();
+		$aliases = $criteria->getAsColumns();
 
-        // get the first part of the SQL statement, the SELECT part
-        $selectSql = $db->createSelectSqlPart($criteria, $fromClause);
+		// simple copy
+		$selectModifiers = $criteria->getSelectModifiers();
 
-        // Handle joins
-        // joins with a null join type will be added to the FROM clause and the condition added to the WHERE clause.
-        // joins of a specified type: the LEFT side will be added to the fromClause and the RIGHT to the joinClause
-        foreach ($criteria->getJoins() as $join) {
+		// get selected columns
+		foreach ($select as $columnName) {
 
-            $join->setDB($db);
+			// expect every column to be of "table.column" formation
+			// it could be a function:  e.g. MAX(books.price)
 
-            // add 'em to the queues..
-            if (!$fromClause) {
-                $fromClause[] = $join->getLeftTableWithAlias();
-            }
-            $joinTables[] = $join->getRightTableWithAlias();
-            $joinClause[] = $join->getClause($params);
-        }
+			$tableName = null;
 
-        // add the criteria to WHERE clause
-        // this will also add the table names to the FROM clause if they are not already
-        // included via a LEFT JOIN
-        foreach ($criteria->keys() as $key) {
+			$selectClause[] = $columnName; // the full column name: e.g. MAX(books.price)
 
-            $criterion = $criteria->getCriterion($key);
-            $table = null;
-            foreach ($criterion->getAttachedCriterion() as $attachedCriterion) {
-                $tableName = $attachedCriterion->getTable();
+			$parenPos = strrpos($columnName, '(');
+			$dotPos = strrpos($columnName, '.', ($parenPos !== false ? $parenPos : 0));
 
-                $table = $criteria->getTableForAlias($tableName);
-                if ($table !== null) {
-                    $fromClause[] = $table . ' ' . $tableName;
-                } else {
-                    $fromClause[] = $tableName;
-                    $table = $tableName;
-                }
+			// [HL] I think we really only want to worry about adding stuff to
+			// the fromClause if this function has a TABLE.COLUMN in it at all.
+			// e.g. COUNT(*) should not need this treatment -- or there needs to
+			// be special treatment for '*'
+			if ($dotPos !== false) {
 
-                if (($criteria->isIgnoreCase() || $attachedCriterion->isIgnoreCase())
-                && $dbMap->getTable($table)->getColumn($attachedCriterion->getColumn())->isText()) {
-                    $attachedCriterion->setIgnoreCase(true);
-                }
-            }
+				if ($parenPos === false) { // table.column
+					$tableName = substr($columnName, 0, $dotPos);
+				} else { // FUNC(table.column)
+					$tableName = substr($columnName, $parenPos + 1, $dotPos - ($parenPos + 1));
+					// functions may contain qualifiers so only take the last
+					// word as the table name.
+					// COUNT(DISTINCT books.price)
+					$lastSpace = strpos($tableName, ' ');
+					if ($lastSpace !== false) { // COUNT(DISTINCT books.price)
+						$tableName = substr($tableName, $lastSpace + 1);
+					}
+				}
+				$tableName2 = $criteria->getTableForAlias($tableName);
+				if ($tableName2 !== null) {
+					$fromClause[] = $tableName2 . ' ' . $tableName;
+				} else {
+					$fromClause[] = $tableName;
+				}
 
-            $criterion->setDB($db);
+			} // if $dotPost !== null
+		}
 
-            $sb = '';
-            $criterion->appendPsTo($sb, $params);
-            $whereClause[] = $sb;
-        }
+		// set the aliases
+		foreach ($aliases as $alias => $col) {
+			$selectClause[] = $col . " AS " . $alias;
+		}
 
-        // Unique from clause elements
-        $fromClause = array_unique($fromClause);
-        $fromClause = array_diff($fromClause, array(''));
+		// add the criteria to WHERE clause
+		// this will also add the table names to the FROM clause if they are not already
+		// invluded via a LEFT JOIN
+		foreach ($criteria->keys() as $key) {
 
-        // tables should not exist in both the from and join clauses
-        if ($joinTables && $fromClause) {
-            foreach ($fromClause as $fi => $ftable) {
-                if (in_array($ftable, $joinTables)) {
-                    unset($fromClause[$fi]);
-                }
-            }
-        }
+			$criterion = $criteria->getCriterion($key);
+			$someCriteria = $criterion->getAttachedCriterion();
+			$someCriteriaLength = count($someCriteria);
+			$table = null;
+			for ($i=0; $i < $someCriteriaLength; $i++) {
+				$tableName = $someCriteria[$i]->getTable();
 
-        // Add the GROUP BY columns
-        $groupByClause = $groupBy;
+				$table = $criteria->getTableForAlias($tableName);
+				if ($table !== null) {
+					$fromClause[] = $table . ' ' . $tableName;
+				} else {
+					$fromClause[] = $tableName;
+					$table = $tableName;
+				}
 
-        $having = $criteria->getHaving();
-        $havingString = null;
-        if ($having !== null) {
-            $sb = '';
-            $having->appendPsTo($sb, $params);
-            $havingString = $sb;
-        }
+				$ignoreCase =
+				(($criteria->isIgnoreCase()
+				|| $someCriteria[$i]->isIgnoreCase())
+				&& (strpos($dbMap->getTable($table)->getColumn($someCriteria[$i]->getColumn())->getType(), "VARCHAR") !== false)
+				);
 
-        if (!empty($orderBy)) {
+				$someCriteria[$i]->setIgnoreCase($ignoreCase);
+			}
 
-            foreach ($orderBy as $orderByColumn) {
+			$criterion->setDB($db);
 
-                // Add function expression as-is.
+			$sb = "";
+			$criterion->appendPsTo($sb, $params);
+			$whereClause[] = $sb;
+		}
 
-                if (strpos($orderByColumn, '(') !== false) {
-                    $orderByClause[] = $orderByColumn;
-                    continue;
-                }
+		// Handle joins
+		// joins with a null join type will be added to the FROM clause and the condition added to the WHERE clause.
+		// joins of a specified type: the LEFT side will be added to the fromClause and the RIGHT to the joinClause
+		foreach ((array) $criteria->getJoins() as $join) { 
+			// The join might have been established using an alias name
+			$leftTable = $join->getLeftTableName();
+			$leftTableAlias = '';
+			if ($realTable = $criteria->getTableForAlias($leftTable)) {
+				$leftTableAlias = " $leftTable";
+				$leftTable = $realTable;
+			}
 
-                // Split orderByColumn (i.e. "table.column DESC")
+			$rightTable = $join->getRightTableName();
+			$rightTableAlias = '';
+			if ($realTable = $criteria->getTableForAlias($rightTable)) {
+				$rightTableAlias = " $rightTable";
+				$rightTable = $realTable;
+			}
 
-                $dotPos = strrpos($orderByColumn, '.');
+			// determine if casing is relevant.
+			if ($ignoreCase = $criteria->isIgnoreCase()) {
+				$leftColType = $dbMap->getTable($leftTable)->getColumn($join->getLeftColumnName())->getType();
+				$rightColType = $dbMap->getTable($rightTable)->getColumn($join->getRightColumnName())->getType();
+				$ignoreCase = ($leftColType == 'string' || $rightColType == 'string');
+			}
 
-                if ($dotPos !== false) {
-                    $tableName = substr($orderByColumn, 0, $dotPos);
-                    $columnName = substr($orderByColumn, $dotPos + 1);
-                } else {
-                    $tableName = '';
-                    $columnName = $orderByColumn;
-                }
+			// build the condition
+			$condition = '';
+			foreach ($join->getConditions() as $index => $conditionDesc)
+			{
+				if ($ignoreCase) {
+					$condition .= $db->ignoreCase($conditionDesc['left']) . $conditionDesc['operator'] . $db->ignoreCase($conditionDesc['right']);
+				} else {
+					$condition .= implode($conditionDesc);
+				}
+				if ($index + 1 < $join->countConditions()) {
+					$condition .= ' AND ';
+				}
+			}
 
-                $spacePos = strpos($columnName, ' ');
+			// add 'em to the queues..
+			if ($joinType = $join->getJoinType()) {
+			  // real join
+				if (!$fromClause) {
+					$fromClause[] = $leftTable . $leftTableAlias;
+				}
+				$joinTables[] = $rightTable . $rightTableAlias;
+				$joinClause[] = $join->getJoinType() . ' ' . $rightTable . $rightTableAlias . " ON ($condition)";
+			} else {
+			  // implicit join, translates to a where
+				$fromClause[] = $leftTable . $leftTableAlias;
+				$fromClause[] = $rightTable . $rightTableAlias;
+				$whereClause[] = $condition;
+			}
+		}
 
-                if ($spacePos !== false) {
-                    $direction = substr($columnName, $spacePos);
-                    $columnName = substr($columnName, 0, $spacePos);
-                } else {
-                    $direction = '';
-                }
+		// Unique from clause elements
+		$fromClause = array_unique($fromClause);
+		$fromClause = array_diff($fromClause, array(''));
+		
+		// tables should not exist in both the from and join clauses
+		if ($joinTables && $fromClause) {
+			foreach ($fromClause as $fi => $ftable) {
+				if (in_array($ftable, $joinTables)) {
+					unset($fromClause[$fi]);
+				}
+			}
+		}
 
-                $tableAlias = $tableName;
-                if ($aliasTableName = $criteria->getTableForAlias($tableName)) {
-                    $tableName = $aliasTableName;
-                }
+		// Add the GROUP BY columns
+		$groupByClause = $groupBy;
 
-                $columnAlias = $columnName;
-                if ($asColumnName = $criteria->getColumnForAs($columnName)) {
-                    $columnName = $asColumnName;
-                }
+		$having = $criteria->getHaving();
+		$havingString = null;
+		if ($having !== null) {
+			$sb = "";
+			$having->appendPsTo($sb, $params);
+			$havingString = $sb;
+		}
 
-                $column = $tableName ? $dbMap->getTable($tableName)->getColumn($columnName) : null;
+		if (!empty($orderBy)) {
 
-                if ($criteria->isIgnoreCase() && $column && $column->isText()) {
-                    $ignoreCaseColumn = $db->ignoreCaseInOrderBy("$tableAlias.$columnAlias");
-                    $orderByClause[] = $ignoreCaseColumn . $direction;
-                    $selectSql .= ', ' . $ignoreCaseColumn;
-                } else {
-                    $orderByClause[] = $orderByColumn;
-                }
-            }
-        }
+			foreach ($orderBy as $orderByColumn) {
 
-        if (empty($fromClause) && $criteria->getPrimaryTableName()) {
-            $fromClause[] = $criteria->getPrimaryTableName();
-        }
+				// Add function expression as-is.
 
-        // tables should not exist as alias of subQuery
-        if ($criteria->hasSelectQueries()) {
-            foreach ($fromClause as $key => $ftable) {
-                if (strpos($ftable, ' ') !== false) {
-                    list($realtable, $tableName) = explode(' ', $ftable);
-                } else {
-                    $tableName = $ftable;
-                }
-                if ($criteria->hasSelectQuery($tableName)) {
-                    unset($fromClause[$key]);
-                }
-            }
-        }
+				if (strpos($orderByColumn, '(') !== false) {
+					$orderByClause[] = $orderByColumn;
+					continue;
+				}
 
-        // from / join tables quoted if it is necessary
-        if ($db->useQuoteIdentifier()) {
-            $fromClause = array_map(array($db, 'quoteIdentifierTable'), $fromClause);
-            $joinClause = $joinClause ? $joinClause : array_map(array($db, 'quoteIdentifierTable'), $joinClause);
-        }
+				// Split orderByColumn (i.e. "table.column DESC")
 
-        // add subQuery to From after adding quotes
-        foreach ($criteria->getSelectQueries() as $subQueryAlias => $subQueryCriteria) {
-            $fromClause[] = '(' . BasePeer::createSelectSql($subQueryCriteria, $params) . ') AS ' . $subQueryAlias;
-        }
+				$dotPos = strrpos($orderByColumn, '.');
 
-        // build from-clause
-        $from = '';
-        if (!empty($joinClause) && count($fromClause) > 1) {
-            $from .= implode(" CROSS JOIN ", $fromClause);
-        } else {
-            $from .= implode(", ", $fromClause);
-        }
+				if ($dotPos !== false) {
+					$tableName = substr($orderByColumn, 0, $dotPos);
+					$columnName = substr($orderByColumn, $dotPos+1);
+				}
+				else {
+					$tableName = '';
+					$columnName = $orderByColumn;
+				}
 
-        $from .= $joinClause ? ' ' . implode(' ', $joinClause) : '';
+				$spacePos = strpos($columnName, ' ');
 
-        // Build the SQL from the arrays we compiled
-        $sql =  $selectSql
-        ." FROM "  . $from
-        .($whereClause ? " WHERE ".implode(" AND ", $whereClause) : "")
-        .($groupByClause ? " GROUP BY ".implode(",", $groupByClause) : "")
-        .($havingString ? " HAVING ".$havingString : "")
-        .($orderByClause ? " ORDER BY ".implode(",", $orderByClause) : "");
+				if ($spacePos !== false) {
+					$direction = substr($columnName, $spacePos);
+					$columnName = substr($columnName, 0, $spacePos);
+				}
+				else {
+					$direction = '';
+				}
 
-        // APPLY OFFSET & LIMIT to the query.
-        if ($criteria->getLimit() || $criteria->getOffset()) {
-            $db->applyLimit($sql, $criteria->getOffset(), $criteria->getLimit(), $criteria);
-        }
+				$tableAlias = $tableName;
+				if ($aliasTableName = $criteria->getTableForAlias($tableName)) {
+					$tableName = $aliasTableName;
+				}
 
-        return $sql;
+				$columnAlias = $columnName;
+				if ($asColumnName = $criteria->getColumnForAs($columnName)) {
+					$columnName = $asColumnName;
+				}
+
+				$column = $tableName ? $dbMap->getTable($tableName)->getColumn($columnName) : null;
+
+				if ($criteria->isIgnoreCase() && $column && $column->isText()) {
+					$orderByClause[] = $db->ignoreCaseInOrderBy("$tableAlias.$columnAlias") . $direction;
+					$selectClause[] = $db->ignoreCaseInOrderBy("$tableAlias.$columnAlias");
+				} else {
+					$orderByClause[] = $orderByColumn;
+				}
+			}
+		}
+
+		if (empty($fromClause) && $criteria->getPrimaryTableName()) {
+			$fromClause[] = $criteria->getPrimaryTableName();
+		}
+
+		// from / join tables quoten if it is necessary
+		if ($db->useQuoteIdentifier()) {
+			$fromClause = array_map(array($db, 'quoteIdentifierTable'), $fromClause);
+			$joinClause = $joinClause ? $joinClause : array_map(array($db, 'quoteIdentifierTable'), $joinClause);
+		}
+
+		// build from-clause
+		$from = '';
+		if (!empty($joinClause) && count($fromClause) > 1) {
+			$from .= implode(" CROSS JOIN ", $fromClause);
+		} else {
+			$from .= implode(", ", $fromClause);
+		}
+		
+		$from .= $joinClause ? ' ' . implode(' ', $joinClause) : '';
+
+		// Build the SQL from the arrays we compiled
+		$sql =  "SELECT "
+		.($selectModifiers ? implode(" ", $selectModifiers) . " " : "")
+		.implode(", ", $selectClause)
+		." FROM "  . $from
+		.($whereClause ? " WHERE ".implode(" AND ", $whereClause) : "")
+		.($groupByClause ? " GROUP BY ".implode(",", $groupByClause) : "")
+		.($havingString ? " HAVING ".$havingString : "")
+		.($orderByClause ? " ORDER BY ".implode(",", $orderByClause) : "");
+
+		// APPLY OFFSET & LIMIT to the query.
+		if ($criteria->getLimit() || $criteria->getOffset()) {
+			$db->applyLimit($sql, $criteria->getOffset(), $criteria->getLimit());
+		}
+
+		return $sql;
     }
 
     /**
